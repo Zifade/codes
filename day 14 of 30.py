e061fc3a7f8b0a05e9bd7eab1224ce9e0f098ff9
Jorge Lutz <jorge.lutz1@gmail.com>
2024-12-31 15:43:28 -0300

    algunos hechos

--- day 14 of 30.py
+++ day 14 of 30.py
@@ -1,14 +1,47 @@
+from functools import reduce
+from countries_data import paises
 #---Exercises LVL1
 
 #Explain the difference between map, filter, and reduce.
+"""
+Son funciones de alto orden en python que tienen utilidades distintas, si bien las 3 son iterativas, es decir, recorren una lista, tienen
+objetivos distintos.
+map() itera una lista dada y aplica un metodo(def) dado tambien, a todos los elementos de la lista.
+filter() itera una lista dada a traves de un metodo(def) y discrimina entre true or false, por lo que se ha de establecer un criterio
+condicional con result true or false.
+reduce() itera una lista dada y lo corre a traves de un metodo(def) dado, pero retorna un valor unico y no una lista como las anteriores 
+"""
 
 #Explain the difference between higher order function, closure and decorator
+"""
+Las funciones de alto orden son metodos(def), que son capaces capaces de entregar otros metodos(def) como resultado.
+
+un decorador es una funcion de alto orden que permite modificar el resultado de la funcion sin modificar su estructura, es decir, se añade
+un tinte a una tela. La tela sigue funcionando como tela pero ahora posee un color.(por ejemplo capitalize()el resuldato de un metodos(def))
+
+Enclosure es la concatenacion de un metodos(def) dentro de otro metodos(def), permitiendo la ejecucion de ambos metodos(def) con solo llamar
+al contenedor
+"""
 
 #Define a call function before map, filter or reduce, see examples.
+"""
+persons=['house','amber','chase','foreman','wilson','cameron']
+def greet(p):
+    
+    return 'Hola {}, como estas?'.format(p)
+greeting=map(greet, persons)
+print(list(greeting))
+"""
 
 #Use for loop to print each country in the countries list.
-
+paises
+def enumeracion_paises(p):
+    return 'Pais {}'.format(p)
+numero_pais= map(enumeracion_paises, paises)
+for pais in numero_pais:
+    print(pais)
 #Use for to print each name in the names list.
+
 
 #Use for to print each number in the numberslis
 
