6759e567d629d566071655c636f6b3049b2b21db
Jorge Lutz <jorge.lutz1@gmail.com>
2025-01-24 22:54:21 -0300

    exs 1 finnished

--- day 18 of 30.py
+++ day 18 of 30.py
@@ -1,7 +1,22 @@
+import re
+from collections import Counter
 #---Exercises LVL 1
 
 #What is the most frequent word in the following paragraph?
 paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.'
+
+def count_words_regex(para):
+    para = para.lower()
+    words = re.findall(r'\b\w+\b', para)
+    word_counts = {}
+    for word in words:
+        word_counts[word] = word_counts.get(word, 0) + 1
+    most_common = max(word_counts.items(), key=lambda x: x[1])
+    return most_common
+
+palabra_comun = count_words_regex(paragraph)
+print(f"Palabra más común: '{palabra_comun[0]}' (aparece {palabra_comun[1]} veces)")
+
 
 #The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 
 # 4 and 8 in the positive direction. Extract these numbers from this whole text and find the distance between the two furthest particles.
