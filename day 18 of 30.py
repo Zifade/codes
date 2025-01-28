67c8c6aef2568f14f32bb65a62b6912563be54e1
Jorge Lutz <jorge.lutz1@gmail.com>
2025-01-27 21:45:25 -0300

    change

--- day 18 of 30.py
+++ day 18 of 30.py
@@ -20,9 +20,12 @@ print(f"Palabra más común: '{palabra_comun[0]}' (aparece {palabra_comun[1]} ve
 
 #The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 
 # 4 and 8 in the positive direction. Extract these numbers from this whole text and find the distance between the two furthest particles.
+"""
 points = ['-12', '-4', '-3', '-1', '0', '4', '8']
 sorted_points =  [-12, -4, -3, -1, -1, 0, 2, 4, 8]
 distance = 8 -(-12) # 20
+"""
+
 
 #---Exercises LVL 2
 
