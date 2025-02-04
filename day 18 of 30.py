c775a7a93c6f4ef717f762dbe70cb050e7e0a096
Jorge Lutz <jorge.lutz1@gmail.com>
2025-02-03 21:45:33 -0300

    exs lvl2 finnished

--- day 18 of 30.py
+++ day 18 of 30.py
@@ -5,6 +5,7 @@ from collections import Counter
 #What is the most frequent word in the following paragraph?
 paragraph = 'I love teaching. If you do not love teaching what else can you love. I love Python if you do not love something which can give you all the capabilities to develop an application what else can you love.'
 
+"""
 def count_words_regex(para):
     para = para.lower()
     words = re.findall(r'\b\w+\b', para)
@@ -16,24 +17,35 @@ def count_words_regex(para):
 
 palabra_comun = count_words_regex(paragraph)
 print(f"Palabra más común: '{palabra_comun[0]}' (aparece {palabra_comun[1]} veces)")
-
-
-#The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 
-# 4 and 8 in the positive direction. Extract these numbers from this whole text and find the distance between the two furthest particles.
-"""
-points = ['-12', '-4', '-3', '-1', '0', '4', '8']
-sorted_points =  [-12, -4, -3, -1, -1, 0, 2, 4, 8]
-distance = 8 -(-12) # 20
 """
 
+
+
+text='The position of some particles on the horizontal x-axis are -12, -4, -3 and -1 in the negative direction, 0 at origin, 4 and 8 in the positive direction. Extract these numbers from this whole text and find the distance between the two furthest particles.'
+"""
+positions= re.findall(r'-?\d+\.?\d*', text)
+print(positions)
+positions_int=list(map(int, positions))
+print(positions_int)
+distance_delta=positions_int[0]- positions_int[-1]
+if distance_delta<0:
+    distance_delta= distance_delta*-1
+else:
+    distance_delta
+print(distance_delta)
+"""
 
 #---Exercises LVL 2
 
 #Write a pattern which identifies if a string is a valid python variable
 """
+def is_valid_variable(variable):
+    pattern = r'^[a-zA-Z_][a-zA-Z0-9_]*$'
+    return bool(re.match(pattern, variable))
+
 is_valid_variable('first_name') # True
 is_valid_variable('first-name') # False
-is_valid_variable('1first_name') # False
+print(is_valid_variable('1first_name')) # False
 is_valid_variable('firstname') # True
 """
 
