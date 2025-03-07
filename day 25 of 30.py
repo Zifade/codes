41faff4d50c9c854bdc0326b522f989e0b31ff36
Jorge Lutz <jorge.lutz1@gmail.com>
2025-03-07 19:49:40 -0300

    exs

--- day 25 of 30.py
+++ day 25 of 30.py
@@ -1,9 +1,24 @@
+import pandas as pd
+import numpy  as np
 
 #Read the hacker_news.csv file from data directory
 #Get the first five rows
+"print(df.head())"
 #Get the last five rows
+"print(df.tail())"
 #Get the title column as pandas series
 #Count the number of rows and columns
     #Filter the titles which contain python
     #Filter the titles which contain JavaScript
     #Explore the data and make sense of it
+
+
+data = [
+    {'Name': 'Asabeneh', 'Country': 'Finland', 'City': 'Helsinki'},
+    {'Name': 'David', 'Country': 'UK', 'City': 'London'},
+    {'Name': 'John', 'Country': 'Sweden', 'City': 'Stockholm'},
+    {'Name': 'Zidane', 'Country': 'Kingdom', 'City': 'Alexandria'}]
+df = pd.DataFrame(data)
+print(df)
+print(df.shape)
+
