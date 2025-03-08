a8ec633e3380436dddc373dfc173df91f547f9b6
Jorge Lutz <jorge.lutz1@gmail.com>
2025-03-08 17:37:54 -0300

    day 25 finnished

--- day 25 of 30.py
+++ day 25 of 30.py
@@ -1,24 +1,49 @@
 import pandas as pd
 import numpy  as np
 
+
 #Read the hacker_news.csv file from data directory
+with open(r'data\hacker_news.csv') as text:
+    hack_df=pd.read_csv(text)
+"print(hack_df)"
+
 #Get the first five rows
-"print(df.head())"
+
+"print('Las primeras 5 filas: \n', hack_df.head())"
+
+
 #Get the last five rows
-"print(df.tail())"
+"print('Las ultimas 5 filas: \n',hack_df.tail())"
+
 #Get the title column as pandas series
+"print('las columnas son: \n', hack_df.columns)"
+
 #Count the number of rows and columns
+"print('Numero de columnas y filas: ', hack_df.shape)"
+
+#--
+
     #Filter the titles which contain python
+
+python_rows = hack_df[hack_df['title'].str.contains('python', case=False)]
+
+for index, row in python_rows.iterrows():
+    print(f"ID: {row['id']}, Título: {row['title']}")
+
     #Filter the titles which contain JavaScript
+
+js_rows = hack_df[hack_df['title'].str.contains('javascript', case=False)]
+
+for index, row in js_rows.iterrows():
+    print(f"ID: {row['id']}, Título: {row['title']}")
+
     #Explore the data and make sense of it
+if len(python_rows)> len(js_rows):
+    print('Python es más comun en las noticias de hacking \n Python news: ', len(python_rows), 'JavaScript news:', len(js_rows))
+if len(python_rows)< len(js_rows):
+    print('JavaScript es más comun en las noticias de hacking \n Python news: ', len(python_rows), 'JavaScript news:', len(js_rows))
+if len(python_rows) == len(js_rows):
+    print('Python y JavaSript son igual de comunes en las noticias de hacking \n Python news: ', len(python_rows), 'JavaScript news:', len(js_rows))
 
 
-data = [
-    {'Name': 'Asabeneh', 'Country': 'Finland', 'City': 'Helsinki'},
-    {'Name': 'David', 'Country': 'UK', 'City': 'London'},
-    {'Name': 'John', 'Country': 'Sweden', 'City': 'Stockholm'},
-    {'Name': 'Zidane', 'Country': 'Kingdom', 'City': 'Alexandria'}]
-df = pd.DataFrame(data)
-print(df)
-print(df.shape)
 
