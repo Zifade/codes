7b00b07e2ff3f8fa1eb754a26d91afa62952efd6
Jorge Lutz <jorge.lutz1@gmail.com>
2025-03-09 15:37:02 -0300

    innit flask project

--- day 26 of 30.py
+++ day 26 of 30.py
@@ -1,1 +1,6 @@
 #You will build this application. Only the text analyser part is left
+#   localizado en la carpeta "python_for_web"
+
+#iniciacion de flask
+#   python -m venv venv(dentro de la carpeta objetivo)
+#   source venv/bin/activate(en la misma ruta)
