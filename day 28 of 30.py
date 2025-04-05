5f4affcbea562746d57548449bdea57c4c2c717c
Jorge Lutz <jorge.lutz1@gmail.com>
2025-04-05 19:50:20 -0300

    day 28 finnished

--- day 28 of 30.py
+++ day 28 of 30.py
@@ -1,1 +1,11 @@
 #Read about API and HTTP
+
+# API
+#https://www.youtube.com/watch?v=u2Ms34GE14U
+#https://www.youtube.com/watch?v=rMPLHPnltmM
+#https://www.youtube.com/watch?v=7OXs3kUjdNE
+
+# HTTP
+#https://youtu.be/60606AHuq8c
+#https://www.youtube.com/watch?v=70fwt29crLs
+#https://www.youtube.com/watch?v=VROImVct7YA
