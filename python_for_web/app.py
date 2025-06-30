33990027e14751017bc763f50af1961cb2db6e5e
Jorge Lutz <jorge.lutz1@gmail.com>
2025-06-30 19:15:25 -0400

    time10s y get_db optimizado

--- python_for_web/app.py
+++ python_for_web/app.py
@@ -22,8 +22,16 @@ def get_db():
     if _client is None:
         MONGODB_URI = os.environ.get('MONGODB_URI', 'mongodb+srv://jorgelutz1:XHIz5HzUCo1cXJ7t@30daysofpython.e9nc9fv.mongodb.net/thirty_days_of_python')
         
-        # Conexión ultra-simple
-        _client = pymongo.MongoClient(MONGODB_URI)
+        # Conexión ultra-rápida
+        _client = pymongo.MongoClient(
+            MONGODB_URI,
+            serverSelectionTimeoutMS=3000,  # 3 segundos máximo
+            connectTimeoutMS=3000,
+            socketTimeoutMS=3000,
+            maxPoolSize=1,
+            minPoolSize=0,
+            maxIdleTimeMS=30000
+        )
         _db = _client.thirty_days_of_python
     return _db
 
